51b8a9bf27d47965d58d834b1207a274f52a995c
Jazzenkrish <152978226+Jazzenkrish@users.noreply.github.com>
2024-02-26 05:09:24 -0600

    Update Entidad.py

--- Entidad.py
+++ Entidad.py
@@ -1,21 +1,21 @@
 import random
-
 class Entidad:
-    def __init__(self, nombre, mensaje, curva, G, orden_G, tabla):
+    def __init__(self, nombre, mensaje, curva, G, orden_G, llave_privada=None, punto_privado=None):
         self.nombre = nombre
         self.mensaje = mensaje
         self.curva = curva
         self.G = G
         self.orden_G = orden_G
-        self.llave_privada = random.randint(1, orden_G)
-        self.punto_privado = random.choice(curva.puntos())
-        self.llaves_publicas = []
+        self.llave_privada = llave_privada if llave_privada is not None else random.randint(1, orden_G)
+        if punto_privado is None:
+            self.punto_privado = self.curva.mult(self.llave_privada, G)  
+        else:
+            self.punto_privado = punto_privado
+        self.pks= []
         self.llaves_recibidas = []
-        self.tabla = tabla
 
     def __str__(self):
-        return f"Entidad: {self.nombre}, Mensaje: {self.mensaje}, Curva Eliptica: {str(self.curva)}, Punto Generador: {str(self.G)}, Orden de G: {self.orden_G}, Llave Privada: {self.llave_privada}, Punto Privado: {str(self.punto_privado)}, Llaves Publicas: {self.llaves_publicas}, Llaves Recibidas: {self.llaves_recibidas}"
-
+      return f"Entidad: {self.nombre}, Mensaje: {self.mensaje}, Curva Eliptica: {str(self.curva)}, Punto Generador: {str(self.G)}, Orden de G: {self.orden_G}, Llave Privada: {self.llave_privada}, Punto Privado: {str(self.punto_privado)}, Llaves Publicas: {self.pks}, Llaves Recibidas: {self.llaves_recibidas}"
     def cifrar(self):
         # Aquí va la implementación de cifrado
         pass
@@ -25,13 +25,24 @@ class Entidad:
         pass
 
     def generaLlavesPublicas(self):
-        # Aquí va la implementación de generación de llaves públicas
-        pass
+        C_mas_A = self.curva.suma(self.G, self.punto_privado)
+        A1 = self.curva.mult(self.llave_privada, C_mas_A)
+        A2 = self.curva.mult(self.llave_privada, self.punto_privado)
+        self.pks.append(A1)
+        self.pks.append(A2)
 
     def recibeLlavesPublicas(self, pks):
-        # Aquí va la implementación de recepción de llaves públicas
-        pass
+      if isinstance(pks, list) and len(pks) >= 2:
+         self.llaves_recibidas.extend(pks)  
+      else:
+        raise ValueError("Se deben proporcionar un par de llaves públicas.")
 
     def llavesFinales(self):
-        # Aquí va la implementación de generación de llaves finales
-        pass
+      if len(self.llaves_recibidas) == 2:
+          Pk2 = self.llaves_recibidas[1]
+          Pke = self.curva.mult(self.llave_privada, Pk2)
+          self.pks.append(Pke)
+          print(f"Llave final de {self.nombre}: {Pke}")
+          return self.pks
+      else:
+        raise ValueError("Error: No se han recibido suficientes llaves públicas para generar la llave final.")
